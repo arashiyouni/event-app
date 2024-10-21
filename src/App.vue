abe81d3fb7da7b3314e19c5442a441761c03926f
angie espinoza <angie.espinoza.campos@gmail.com>
2024-10-21 08:13:41 -0600

    initial project with vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    mounted(){
+        console.log("Oli", import.meta.env.VITE_API_EVENT)
+    }
+}
+
+</script>
+
+<template>
+<h1>Hola</h1>
+
+</template>
+
+<style>
+</style>
---
+++ src/store/event.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+
+    state: {
+        allEvent: [],
+        error: null
+    },
+
+    mutations: {
+      
+    },
+
+    actions: {
+        GET_ALL_EVENTS({commit}){
+            try{
+
+            }catch(err){
+
+            }
+        }
+    }
+})
