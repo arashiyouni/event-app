e9673fbb27bf08fa6c318553bf2962e820fdac02
arashiyouni <angie.espinoza.campos@gmail.com>
2024-10-22 22:32:10 -0600

    build setup vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
     <h1>Hola</h1>
-   <h3>{{  event }}</h3>
+    <button class="rounded-full bg-orange-300">Save Changes</button>
+    <v-btn>
+        OLAAA
+    </v-btn>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+
 export default {
     name: 'Main App',
     computed: {
-        ...mapState({
-            allEvent: (state) => state.allEvent,
-            event: (state) => state.event
-        })
+        // ...mapState({
+        //     allEvent: (state) => state.allEvent,
+        //     event: (state) => state.event
+        // })
     },
     methods: {
-        ...mapActions(['GET_ALL_EVENTS', 'GET_EVENT']), 
+        // ...mapActions(['GET_ALL_EVENTS', 'GET_EVENT']), 
 
-        async loadEvents() {
-            await this.GET_ALL_EVENTS(); 
-        },
-        async getEvents() {
-            await this.GET_EVENT(); 
-        },
+        // async loadEvents() {
+        //     await this.GET_ALL_EVENTS(); 
+        // },
+        // async getEvents() {
+        //     await this.GET_EVENT(); 
+        // },
     },
-    mounted(){
-            this.loadEvents()
-            this.getEvents()
-    }
+    // mounted(){
+    //         this.loadEvents()
+    // }
 
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./style.css";
+// Importa Vuetify y los estilos necesarios
+import { createVuetify } from "vuetify";
+import "vuetify/styles"; // Estilos globales de Vuetify
+import "@mdi/font/css/materialdesignicons.css"; // Iconos
 
-const app = createApp(App)
+// Crea la instancia de Vuetify
+const vuetify = createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#1E88E5",
+          secondary: "#26C6DA",
+        },
+      },
+    },
+  },
+});
 
-app.use(router)
-app.use(store)
-app.mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.use(vuetify);
+app.mount("#app");
